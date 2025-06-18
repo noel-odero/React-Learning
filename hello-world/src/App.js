81571862585b4149e882b44865c4544e8dfa597f
Noel Odero <anoel.odero@gmail.com>
2025-06-18 10:52:18 +0200

    jsx learning

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,9 +1,54 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const pets = [
+    {
+      name: 'Dolly',
+      type: 'cat',
+      description: 'Dolly is funny and loves to be around people',
+      skills: ['fetch', 'play dead', 'roll over'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829788/cats/remi-remino-E9kVmtiqqGE-unsplash_uivwfy.jpg',
+    },
+    {
+      name: 'Molly',
+      type: 'cat',
+      description: 'Molly is a sweet and cuddly cat stretch a lot!',
+      skills: ['play dead', 'meow', 'purr'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829829/cats/timo-volz-ZlFKIG6dApg-unsplash_x3xifk.jpg',
+    },
+    {
+      name: 'Molly1',
+      type: 'cat',
+      description: 'Molly is a sweet and cuddly cat stretch a lot!',
+      skills: ['play dead', 'meow', 'purr'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829829/cats/timo-volz-ZlFKIG6dApg-unsplash_x3xifk.jpg',
+    },
+  ];
   return (
     <div className="App">
-      <h1>Hello World!!!</h1>
+      <h1>My Pets</h1>
+      <ul className="container">
+        {
+          pets.map((pet, index) => (
+            <li key={index}>
+              <h2>{pet.name}</h2>
+              <p>{pet.description}</p>
+              <img src={pet.image} alt={pet.name} width="200px" height="200px" />
+
+              <ul className="skills">
+                {
+                  pet.skills.map((skill, index) => (
+                    <li key={skill}>{skill}</li>
+                  ))
+                }
+              </ul>
+            </li>
+          )
+          )
+        }
+          
+      </ul>
     </div>
   );
 }
