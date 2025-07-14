838a6de6340a6162c667ca8ac854bfc5ee980da1
Noel Odero <anoel.odero@gmail.com>
2025-07-14 11:59:47 +0200

    useCallbacks and useMemo

--- greetings/src/App.js
+++ greetings/src/App.js
@@ -1,24 +1,27 @@
-import './App.css';
-// import Greetings from './greetings';
-// import Counter from './count';
-// import Users from './effect'
-// import Focus from './focus';
-// import Demo from './counter';
-// import Form from './form'
-import Stopwatch from './useref'
+import "./App.css";
+import { useState, useCallback } from "react";
+import GreetingBox from "./usecallback";
 
 function App() {
-  return (
-  
-    <div className="App">
-      {/* <Greetings randomMax={8}/>
-      <Demo />
-      <Counter/>
-      <Focus/>
-      <Users/>
-      <Form /> */}
-      <Stopwatch/>
+  const [name, setName] = useState("");
+  const [counter, setCounter] = useState(0);
 
+  const getGreeting = useCallback(() => {
+    return `Hello ${name}.`;
+  }, [name]);
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Enter your name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <GreetingBox getGreeting={getGreeting} />
+
+      {counter}
+      <button onClick={() => setCounter(counter + 1)}>+ by 1</button>
     </div>
   );
 }
